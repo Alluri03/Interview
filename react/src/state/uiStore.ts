aaa142fd94514740fba2a1e69f91f3b48a587a70
Alluri03 <varma03@gmail.com>
2025-08-22 00:16:21 +0530

    Delete react directory

--- react/src/state/uiStore.ts
+++
@@ -1,7 +0,0 @@
-import { create } from "zustand";
-
-type UiState = { mode: "light" | "dark"; toggle: () => void };
-export const useUiTheme = create<UiState>((set) => ({
-  mode: "light",
-  toggle: () => set(s => ({ mode: s.mode === "light" ? "dark" : "light" }))
-}));
