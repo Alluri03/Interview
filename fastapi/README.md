94eff3ce357d9f8a72a52558f3d9dad43fe3f45e
Alluri03 <varma03@gmail.com>
2025-08-22 00:15:53 +0530

    Delete fastapi directory

--- fastapi/README.md
+++
@@ -1,2 +0,0 @@
-# Bosswallah – FastAPI Coding Exercise (15 min)
-See app/main.py for TODOs.
--- fastapi/app/main.py
+++
@@ -1,4 +0,0 @@
-from fastapi import FastAPI
-app=FastAPI()
-@app.get('/')
-async def r(): return {'ok':True}
